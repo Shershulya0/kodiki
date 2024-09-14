ce988dbef479f8df189c0505a7d03463ec141e32
Shershulya0 <elyashepeleva1@gmail.com>
2024-09-14 21:20:52 +0300

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+const wrap = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Вот же он!";
+  gif.src = "https://media1.tenor.com/m/6URuP1_4sp4AAAAC/cat-run-cat-steal.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
